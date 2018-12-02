a8d4c0a6fe2773fd5e1820f7db574ceb2d78db1f
Bob White <robert.wd.white@gmail.com>
2018-12-01 17:34:23 -0800

    Adding more types.

--- day_01.py
+++ day_01.py
@@ -2,15 +2,15 @@ import itertools
 from typing import Sequence
 
 with open('day_01.input', 'r') as f:
-    vals = list(map(int, f.readlines()))
+    vals: Sequence[int] = list(map(int, f.readlines()))
 
 
-def problem_01(input: Sequence[int]):
+def problem_01(input: Sequence[int]) -> int:
     return sum(input)
 
-def problem_02(input: Sequence[int]):
-    current = 0
-    freq = {current}
+def problem_02(input: Sequence[int]) -> int:
+    current: int = 0
+    freq: set = {current}
     for val in itertools.cycle(vals):
         current += val
         if current in freq:
