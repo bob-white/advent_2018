934515dfa7d01215241dde8dd8e5aaf2450964c4
Bob White <robert.wd.white@gmail.com>
2018-12-09 13:41:40 -0600

    Using itertools.

--- day_03.py
+++ day_03.py
@@ -1,30 +1,90 @@
+"""--- Day 3: No Matter How You Slice It ---
+
+The Elves managed to locate the chimney-squeeze prototype fabric for Santa's suit (thanks to someone who helpfully wrote its box IDs on the wall of the warehouse in the middle of the night). Unfortunately, anomalies are still affecting them - nobody can even agree on how to cut the fabric.
+
+The whole piece of fabric they're working on is a very large square - at least 1000 inches on each side.
+
+Each Elf has made a claim about which area of fabric would be ideal for Santa's suit. All claims have an ID and consist of a single rectangle with edges parallel to the edges of the fabric. Each claim's rectangle is defined as follows:
+
+    The number of inches between the left edge of the fabric and the left edge of the rectangle.
+    The number of inches between the top edge of the fabric and the top edge of the rectangle.
+    The width of the rectangle in inches.
+    The height of the rectangle in inches.
+
+A claim like #123 @ 3,2: 5x4 means that claim ID 123 specifies a rectangle 3 inches from the left edge, 2 inches from the top edge, 5 inches wide, and 4 inches tall. Visually, it claims the square inches of fabric represented by # (and ignores the square inches of fabric represented by .) in the diagram below:
+
+...........
+...........
+...#####...
+...#####...
+...#####...
+...#####...
+...........
+...........
+...........
+
+The problem is that many of the claims overlap, causing two or more claims to cover part of the same areas. For example, consider the following claims:
+
+#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2
+
+Visually, these claim the following areas:
+
+........
+...2222.
+...2222.
+.11XX22.
+.11XX22.
+.111133.
+.111133.
+........
+
+The four square inches marked with X are claimed by both 1 and 2. (Claim 3, while adjacent to the others, does not overlap either of them.)
+
+If the Elves all proceed with their own plans, none of them will have enough fabric. How many square inches of fabric are within two or more claims?
+
+Your puzzle answer was 121163.
+--- Part Two ---
+
+Amidst the chaos, you notice that exactly one claim doesn't overlap by even a single square inch of fabric with any other claim. If you can somehow draw attention to it, maybe the Elves will be able to make Santa's suit after all!
+
+For example, in the claims above, only claim 3 is intact after all claims are made.
+
+What is the ID of the only claim that doesn't overlap?
+
+Your puzzle answer was 943.
+
+"""
+
 
 import re
 from collections import defaultdict
-from typing import Sequence, Optional, Dict
+from itertools import product
+from typing import Sequence, Dict, Tuple
+Coordinate = Tuple[int, int]
+Rect = Tuple[int, int, int, int]
 
 with open('day_03.input', 'r') as f:
     vals: Sequence[str] = list(f.readlines())
 
-fabric: Dict[tuple, int] = defaultdict(int)
-rects: Dict[int, tuple] = {}
-
+fabric: Dict[Coordinate, int] = defaultdict(int)
+rects: Dict[int, Rect] = {}
 
 def part_01(vals: Sequence[str]) -> int:
     for line in vals:
         idx, x, y, w, h = map(int, re.findall(r'\d+', line))
         rects[idx] = x, y, w, h
-        for i in range(w):
-            for j in range(h):
-                fabric[((x+i), (y+j))] += 1
+        for i, j in product(range(w), range(h)):
+            fabric[((x+i), (y+j))] += 1
     return sum(i >= 2 for i in fabric.values())
 
 
-def part_02(vals: Sequence[str]) -> Optional[int]:
+def part_02(vals: Sequence[str]) -> int:
     for idx, (x, y, w, h) in rects.items():
-        if all(fabric[(x+i), (y+j)] == 1 for i in range(w) for j in range(h)):
+        if all(fabric[(x+i), (y+j)] == 1 for i, j in product(range(w), range(h))):
             return idx
-    return None
+    return -1
 
 
 print(part_01(vals))
